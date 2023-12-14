0e237bc0a6f46cb7f2bc30e7a175034bb2e7ae55
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-14 03:02:36 +0100

    Update comments in main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// PB5 corresponds to pin 13, which is connected to the on-board "L" led.
+
 int main(void) {
-	DDRB = 0b00100000; // PB5 output
+	DDRB = 0b00100000; // set the PB5 pin to be an output pin
 
 	while (1) {
-		PORTB = 0b00100000; // PB5
+		PORTB = 0b00100000; // set pin PB5
 		_delay_ms(1000);
 
-		PORTB = 0b00000000; // PB5
+		PORTB = 0b00000000; // reset pin PB5
 		_delay_ms(1000);
 	}
 }
